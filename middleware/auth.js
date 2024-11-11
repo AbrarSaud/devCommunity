dce4a432209d40cc9df5575db672877ddc6a37a7
AbrarSaud <abrar.sauod0@gmail.com>
2024-11-11 19:34:12 +0300

    Create profile and get current profile

--- middleware/auth.js
+++ middleware/auth.js
@@ -5,16 +5,17 @@ module.exports = function (req, res, next) {
   // Get token from header
   const token = req.header("x-auth-token");
 
-  // Check if not token
+  // Check if no token
   if (!token) {
-    return res.status(401).json({ msg: "No token ,authorization denied" });
+    return res.status(401).json({ msg: "No token, authorization denied" });
   }
+
   // Verify token
   try {
-    const decoded = jwt.verify(token, config.get("jwtSecret"));
-    req.user = decoded.user;
+    const decoded = jwt.verify(token, config.get("jwtSecret")); // التحقق من التوكن باستخدام المفتاح السري
+    req.user = decoded.user; // تخزين معلومات المستخدم المستخلصة من التوكن
     next();
   } catch (err) {
-    res.status(401).json({ msg: "Token is not verified .." });
+    res.status(401).json({ msg: "Token is not valid" });
   }
 };
